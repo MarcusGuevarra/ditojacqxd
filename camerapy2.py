fe2c3f0f1073e2bd5126d870d8e79e2d2f85f1a3
mhaanxcodes <107126634+mhaanxcodes@users.noreply.github.com>
2024-07-16 17:44:33 +0800

    Update camerapy2.py

--- camerapy2.py
+++ camerapy2.py
@@ -1,45 +1,39 @@
 import cv2
 
 capture_nugget = cv2.VideoCapture(0)
-
 # for higher image resolution
 capture_nugget.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
 capture_nugget.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 capture_nugget.set(cv2.CAP_PROP_FPS, 60)
 
-def filename(mode, currentframe):
+global currentframe
+
+
+def filename(mode):
     image_name = './data/' + mode + '/2_' + str(currentframe) + '.png'
     return image_name
 
-currentframe = 1
-while True:
-    ret, image = capture_nugget.read()
+
+def save_file(mode, image):
+    image_name = filename(mode)
+    cv2.imwrite(image_name, image)
+    print('Saved ' + image_name)
+
+
+def capture_image():
+    image = capture_nugget.read()
     cv2.imshow('Camera', image)
 
-    if cv2.waitKey(1) == ord('x'):
-        orig_name = filename('orig', currentframe)
-        cv2.imwrite(orig_name, image)
-        print('Saved ' + orig_name)
+    save_file('orig', image)
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    save_file('gray', gray_image)
+
+    edge_image = cv2.Canny(image,70,70)
+    save_file('edge', edge_image)
+
+    currentframe += 1
 
 
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        edge_image = cv2.Canny(image,70,70)
-
-
-        gray_name = filename('gray', currentframe)
-        cv2.imwrite(gray_name, gray_image)
-        print('Saved ' + gray_name)
-
-        edge_name = filename('edge', currentframe)
-        cv2.imwrite(edge_name, edge_image)
-        print('Saved ' + edge_name)
-
-
-        currentframe += 1
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-capture_nugget.release()
-cv2.destroyAllWindows()
+def close_camera():
+    capture_nugget.release()
