1b05b6b108210c41bc18ccf2f6fef8f8aac97d25
Rokunaaa <134189980+Rokunaaa@users.noreply.github.com>
2024-07-16 09:51:27 +0800

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -14,12 +14,28 @@ label = tk.Label(root, text="Hello, Tkinter!")
 label.pack(pady=20)
 
 # Create a button
-def on_button_click():
+
+def on_button_click_1():
+    capture()
+
+def on_button_click_2():
+    capture()
+
+def on_button_click_3():
     capture()
 
 
-button = tk.Button(root, text="Click Me", command=on_button_click)
-button.pack(pady=20)
+
+
+button_1 = tk.Button(root, text="Click Me", command=on_button_click_1)
+button_1.pack(pady=30)
+
+button_2 = tk.Button(root, text="Click Me", command=on_button_click_2)
+button_2.pack(pady=30)
+
+button_3 = tk.Button(root, text="Click Me", command=on_button_click_3)
+button_3.pack(pady=30)
+
 
 # Run the application
 root.mainloop()
