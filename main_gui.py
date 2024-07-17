e435177fb10633b3107cff8cd7c3be8139ee6593
mhaanxcodes <107126634+mhaanxcodes@users.noreply.github.com>
2024-07-17 20:54:19 +0800

    Update and rename GUI.py to main_gui.py

---
+++ main_gui.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from cameranugget import camera, capture_image, currentframe, close_camera
+from AImodel import edge_detection, original, grayscale
+
+# Create the main window
+root = tk.Tk()
+root.title("AUTOMATIC FOOD OIL DETECTION")
+
+# Set the size of the window
+root.geometry("750x500")
+root.resizable(True,True)
+
+# Create a label
+label = tk.Label(root, text="Select which type of images to process through the AI:")
+label.pack(pady=20)
+
+camera()
+while True:
+    if currentframe <= 5:
+        camera_button = tk.Button(root, text="CAPTURE", command=capture_image)
+        camera_button.pack(pady=30)
+    else:
+        close_camera()
+        break
+
+
+def image_modes():
+    original_button = tk.Button(root, text="ORIGINAL", command=original)
+    original_button.pack(pady=30)
+
+    gray_button = tk.Button(root, text="GRAYSCALE", command=grayscale)
+    gray_button.pack(pady=30)
+
+    edge_button = tk.Button(root, text="EDGE DETECTION", command=edge_detection)
+    edge_button.pack(pady=30)
+
+
+# Run the application
+root.mainloop()
+
